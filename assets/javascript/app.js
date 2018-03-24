0d441e49d8ed7b7f4c1d691f21bd9e81bcd635c6
Ross Messer <rmesser@perennial.local>
2018-03-24 12:15:21 -0500

    added moment and time functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -56,72 +56,88 @@ $("#signOut").click(function (e) {
         // ...
     });
 }); // this detects the state change and reports if they are logged in out out...
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            // User is signed in.
-            console.log(user.email, "logged in");
-            // ...
-        } else {
-            console.log("logged out")
-            // User is signed out.
-            // ...
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        // User is signed in.
+        console.log(user.email, "logged in");
+        // ...
+    } else {
+        console.log("logged out")
+        // User is signed out.
+        // ...
+    }
+});
+
+// These are the two submit buttons for the pages
+$("#createAccount").submit(signUpActivation);
+$("#login").submit(logInActivation);
+
+// This is an algorithm that will validate email addresses
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+
+
+
+
+// This is the Line between the Maps Code and the Firebase account creation and login (User Authentication)
+
+console.log("AHSOGHASOGHASOGHAS");
+
+var map;
+var directionsDisplay;
+var directionsService;
+function initMap() {
+    console.log("initmap is running")
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    var centerUS = new google.maps.LatLng(39.83333, -98.585522);
+    var mapOptions = {
+        zoom: 4,
+        center: centerUS
+    }
+    console.log("wasaaaaap");
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById('directionsPanel'));
+}
+
+function calcRoute() {
+    var start = $("#start-location").val().trim();
+    var end = $("#end-location").val().trim();
+    var departureDate = $("#date").val().trim();
+    console.log(departureDate);
+    var departureTime = $("#time").val().trim();
+    console.log(departureTime);
+    var departure = departureDate + " " + departureTime;
+    console.log(departure);
+    var dateFormat = 'YYYY-MM-DD hh:mm';
+    var currentTime = moment().format(dateFormat);
+    var diffTime = moment(currentTime, dateFormat).diff(moment(departure), "seconds");
+    var N = (diffTime * 1000) * -1;
+    console.log(N);
+    var date = new Date(Date.now() + N);
+    var DrivingOptions = {
+        departureTime: date,
+    };
+    var request = {
+        origin: start,
+        destination: end,
+        travelMode: 'DRIVING',
+        drivingOptions: DrivingOptions
+    };
+    console.log(directionsService);
+    directionsService.route(request, function (response, status) {
+        if (status == 'OK') {
+            directionsDisplay.setDirections(response);
+            showSteps(response);
         }
     });
 
-// These are the two submit buttons for the pages
-    $("#createAccount").submit(signUpActivation);
-    $("#login").submit(logInActivation);
-
-// This is an algorithm that will validate email addresses
-    function validateEmail(email) {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(email).toLowerCase());
-    }
-
-
-
-
-
-
-    // This is the Line between the Maps Code and the Firebase account creation and login (User Authentication)
-
-    console.log("AHSOGHASOGHASOGHAS");
-
-    var map;
-    var directionsDisplay;
-    var directionsService;
-    function initMap() {
-        console.log("initmap is running")
-        directionsService = new google.maps.DirectionsService();
-        directionsDisplay = new google.maps.DirectionsRenderer();
-        var centerUS = new google.maps.LatLng(39.83333, -98.585522);
-        var mapOptions = {
-            zoom: 4,
-            center: centerUS
-        }
-        console.log("wasaaaaap");
-        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-        directionsDisplay.setMap(map);
-        directionsDisplay.setPanel(document.getElementById('directionsPanel'));
-    }
-
-    function calcRoute() {
-        var start = $("#start-location").val().trim();
-        var end = $("#end-location").val().trim();
-        var request = {
-            origin: start,
-            destination: end,
-            travelMode: 'DRIVING'
-        };
-        console.log(directionsService);
-        directionsService.route(request, function (response, status) {
-            if (status == 'OK') {
-                directionsDisplay.setDirections(response);
-                showSteps(response);
-            }
-        });
- 
-    }
+}
 //this object will contain all data relevant to the requested route
 function showSteps(directionResult) {
 
@@ -148,7 +164,7 @@ function showSteps(directionResult) {
     var endLat = myRoute.end_location.lat();
     var endLng = myRoute.end_location.lng();
     var tripDuration = myRoute.duration.text;
-    
+
 
 
 
@@ -201,7 +217,7 @@ function showSteps(directionResult) {
                     var curObservationLower = curObservation.toLowerCase();
                     console.log(curObservation);
 
-                    $("#weather-conditions").append("<Div>Temperature:"+curTemp+"</Div>")
+                    $("#weather-conditions").append("<Div>Temperature:" + curTemp + "</Div>")
 
 
                 })
